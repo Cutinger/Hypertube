fd7b39ac099fa8073a348b882dc123dca65ca8b0
jeguglie <jeguglie@student.42.fr>
2020-02-26 05:14:06 +0100

    add scroll_to_top / ratings on movie focus

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grow, Container, Paper, Grid, CircularProgress, Backdrop } from '@material-ui/core';
+import { Grow, Container, Paper, Grid, CircularProgress, Backdrop, Fab, useScrollTrigger, Zoom } from '@material-ui/core';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import StarRatings from 'react-star-ratings';
 import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
     movieCover: {
-        backgroundColor: 'white',
-        minHeight: '100%',
-        minWidth: '100%',
-        transform: 'translate(-50%, -50%)',
         display: 'block',
-        position: 'absolute',
-        top: '50%',
-        left: '50%'
+        // width: '100%',
+        height: '100%'
     },
     movieCoverFocus: {
-        backgroundColor: 'white',
-        minHeight: '100%',
-        minWidth: '100%',
-        transform: 'translate(-50%, -50%)',
         display: 'block',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
+        // width: '100%',
+        height: '100%',
         transition: 'all 0.2s ease-in-out',
-        webkitFilter: 'blur(10px)', /* Safari 6.0 - 9.0 */
-        filter: 'blur(10px)'
+        webkitFilter: 'blur(15px)', /* Safari 6.0 - 9.0 */
+        filter: 'blur(15px)'
+    },
+    movieFocusOverlay:{
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        height: '100%',
+        width: '100%',
+        opacity: '0',
+        transition: 'all 0.2s ease-in-out',
     },
     movieCoverContainer: {
         width: '190px',
@@ -37,7 +40,9 @@ const useStyles = makeStyles(theme => ({
         margin: '0',
         '&:hover':{
             transform: 'scale(1.05)',
-            zIndex: '500'
+        },
+        '&:hover $movieFocusOverlay':{
+            opacity: '1'
         }
     },
     containerGridTopMovie: {
@@ -52,6 +57,33 @@ const useStyles = makeStyles(theme => ({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1,
         color: '#fff',
+    },
+    movieTitle: {
+        padding: '1em',
+        color: 'white',
+        textShadow: '1px 1px 5px rgba(0,0,0,0.7)'
+    },
+    movieRating: {
+        marginTop: '-2em',
+        '& .star': {
+            'filter': 'drop-shadow(-1px 6px 3px rgba(50, 50, 0, 0.5))'
+        },
+        '& svg':{
+            'filter': 'drop-shadow(3px 3px 2px rgba(50, 50, 0, 0.2))'
+        }
+    },
+    movieRatingStars: {
+        '.star': {
+            fill: 'red !important',
+        },
+    },
+    rootScroll: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+        '& .MuiFab-secondary':{
+            background: 'rgba(83, 26, 62, 1)'
+        }
       },
 }));
 
@@ -69,24 +101,43 @@ export default function Home(props) {
             })
         }
         getTopMoviesList();
-        console.log(1);
     }, []);
 
-    const handleMouseEnterMovie = (key) => {
-        setMovieFocus(key);
-    }
+    // Movie focus
+    const handleMouseEnterMovie = (key) => { setMovieFocus(key) }
+    const handleMouseLeaveMovie = (key) => { setMovieFocus(false) }
+
+    // Scroll options
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 200,
+    });
+    
+    const handleClick = event => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+        anchor && anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+    };
+
     return (
         <Container className={classes.home} component="main">
             <Backdrop className={classes.backdrop} open={!topMoviesList ? true : false} >
                 <CircularProgress color="inherit" />
             </Backdrop>
+            <Zoom in={trigger}>
+                <div onClick={handleClick} role="presentation" className={classes.rootScroll}>
+                    <Fab color="secondary" size="small" aria-label="scroll back to top">
+                        <KeyboardArrowUpIcon />
+                    </Fab>
+                </div>
+            </Zoom>
             <div className={classes.containerGridTopMovie}>
                 <Grid direction="row" alignItems="flex-start" justify="center" container className={classes.root} spacing={2}>
                     {topMoviesList && topMoviesList.map((obj, key) => {
                         const timeout = "00";
                         const valueTimeout = key.toString();
                         return (
-                            <Grid key={key} item>
+                            <Grid key={key} id={key === 1 ? "back-to-top-anchor" : null} item>
                                 <Grow 
                                     in={true}
                                     style={key > 1 ? { transformOrigin: '0 0 0' } : {}} 
@@ -95,19 +146,34 @@ export default function Home(props) {
                                 >
                                     <Paper elevation={5} className={classes.paper}>
                                         {movieFocus !== key ? 
-                                            <div className={classes.movieCoverContainer} onMouseEnter={() => handleMouseEnterMovie(key)}>
+                                            <div className={classes.movieCoverContainer} onMouseLeave={()=> handleMouseLeaveMovie(key)} onMouseEnter={() => handleMouseEnterMovie(key) }>
                                                 <img
                                                     src={obj.poster_path ? `http://image.tmdb.org/t/p/w185${obj.poster_path}` : 'https://i.ibb.co/hgvJPFb/default-Img-Profile.png'}
                                                     alt={obj.title}
                                                     className={classes.movieCover}
                                                 />
                                             </div> :
-                                            <div className={classes.movieCoverContainer}>
+                                            <div className={classes.movieCoverContainer} onMouseLeave={()=> handleMouseLeaveMovie(key)} onMouseEnter={() => handleMouseEnterMovie(key) }>
                                                 <img
                                                     src={obj.poster_path ? `http://image.tmdb.org/t/p/w185${obj.poster_path}` : 'https://i.ibb.co/hgvJPFb/default-Img-Profile.png'}
                                                     alt={obj.title}
                                                     className={classes.movieCoverFocus}
                                                 />
+                                                <div className={classes.movieFocusOverlay}>
+                                                    <Grid alignItems="center" direction="column" justify="center" container>
+                                                        <Grid item>
+                                                            <h3 className={classes.movieTitle}>{obj.title}</h3>
+                                                        </Grid>
+                                                        <Grid item className={classes.movieRating}>
+                                                            <StarRatings
+                                                                rating={obj.vote_average / 2}
+                                                                starRatedColor="#f7c12d"
+                                                                starDimension="18px"
+                                                                starSpacing="3px"
+                                                            />
+                                                        </Grid>
+                                                    </Grid>
+                                                </div>
                                             </div> 
                                         }
                                     </Paper>
