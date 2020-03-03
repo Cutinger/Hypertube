e1aa40b1dc7e4505c170c7cd6de62772aa4a0415
jeguglie <jeguglie@student.42.fr>
2020-03-03 20:36:41 +0100

    minor update sidebar research

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -10,8 +10,10 @@ import {
     Fab,
     useScrollTrigger,
     Zoom,
-    Typography
+    Typography,
+    TextField
 } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import PlayCircleFilled from '@material-ui/icons/PlayCircleFilled';
 import AddCircle from '@material-ui/icons/AddCircle';
@@ -217,13 +219,29 @@ const useStyles = makeStyles(theme => ({
     sidebarHomeContainer: {
         width: '15em',
         padding: '1.5em'
+    },
+    actorResearch: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: 'white',
+              borderRadius: '10px',
+              opacity: '0',
+              color: 'white'
+            }
+        },
+        '& input::placeholder': {
+            color: 'white'
+        },
+        '& .MuiSvgIcon-root': {
+            color: 'white'
+        }
     }
 }));
 
 
 const PrettoSlider = withStyles({
     root: {
-        color: '#52af77',
+        backgroundColor: 'linear-gradient(-333deg, rgba(32, 122, 244, 0.8) -39%, #0b1123, #0b1123 70%, rgba(240, 38, 120, 0.8) 223% ) !important',
         height: 8,
     },
     thumb: {
@@ -239,13 +257,16 @@ const PrettoSlider = withStyles({
     },
     active: {},
     valueLabel: {
+        fontSize: '0.8em',
         left: 'calc(-50% + 4px)',
     },
     track: {
         height: 8,
         borderRadius: 4,
+        
     },
     rail: {
+
         height: 8,
         borderRadius: 4,
     },
@@ -371,6 +392,8 @@ export default withRouter(function Home(props) {
     };
 
     const Sidebar = () => {
+        const classes = useStyles();
+
         return (
         <Drawer
             variant="persistent"
@@ -383,13 +406,32 @@ export default withRouter(function Home(props) {
         >
             <div className={classes.drawerHeader}>
                 <IconButton onClick={handleDrawerClose}>
-                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon style={{color: 'white'}} fontSize="large"/> : <ChevronRightIcon />}
                 </IconButton>
             </div>
-            <Divider />
+            <Divider style={{backgroundColor: 'rgba(63, 80, 181, 0.22)'}}/>
             <div className={classes.sidebarHomeContainer}>
-                <Typography gutterBottom>Year</Typography>
-                <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
+                <Typography gutterBottom style={{color: 'white'}}>Year</Typography>
+                <PrettoSlider valueLabelDisplay="auto" min={1910} max={2020} defaultValue={2019}/>
+                <Typography gutterBottom style={{color: 'white'}}>Vote</Typography>
+                <PrettoSlider valueLabelDisplay="auto" min={0} max={5} defaultValue={5} step={1} />
+                <Typography gutterBottom style={{color: 'white'}}>With actor(s)</Typography>
+                <Autocomplete
+                    style={{color: 'white'}}
+                    multiple
+                    id="tags-outlined"
+                    options={top100Films}
+                    getOptionLabel={option => option.title}
+                    filterSelectedOptions
+                    renderInput={params => (
+                    <TextField
+                        {...params}
+                        className={classes.actorResearch}
+                        variant="outlined"
+                        placeholder="Favorites"
+                    />
+                    )}
+                />
             </div>
         </Drawer>)
     }
@@ -498,3 +540,107 @@ export default withRouter(function Home(props) {
         </Container>
     )
 });
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+  ];
