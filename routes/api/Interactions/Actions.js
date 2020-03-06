c62655cdb93a14e3d9a1553c0b2ee304aec6f58d
Diyaeddine LAOUID <diyaeddine.laouid@gmail.com>
2020-03-07 00:10:56 +0100

    add Actions
    watchlist & likes

---
+++ routes/api/Interactions/Actions.js
@@ -0,0 +1,58 @@
+const mongoose      = require('mongoose');
+const db            = mongoose.connection;
+const jwt           = require('jsonwebtoken')
+const key           = require('../../../config/keys.js')
+const schema        = require('../../../models/WatchList.js')
+
+const likeInteraction = (req, res) => {
+    var imdbcode = req.params.id
+    if (!req.cookies.token) { return res.sendStatus(403) }
+    var userID = jwt.verify(req.cookies.token, key.secretOrKey).id
+    if (!userID) { return res.sendStatus(403) }
+    Movie.findOne({ imdb_code: imdbcode }, (err, data) => {
+        if (!data)
+            res.sendStatus(403)
+        if (!data.like.includes(userID)) {
+            data.like.push(userID)
+        } else {
+            data.like = data.like.filter(like => like != userID)
+        }
+        data.save( (err) => { console.log(err) })
+    })
+    return res.sendStatus(200)
+}
+
+const watchList = (req, res) => {
+    var imdbcode = req.params.id
+    if (!req.cookies.token) { return res.sendStatus(403) }
+    var userID = jwt.verify(req.cookies.token, key.secretOrKey).id
+    if (!userID) { return res.sendStatus(403) }
+
+    WatchList.findOne({ user_id: userID }, (err, data) => {
+        if (!data) {
+            var addList = new WatchList({
+                user_id:   userID,
+                movies: [ imdbcode ]
+            })
+            addList.save( (err) => { console.log(err) })
+        } else {
+            if (!data.movies.includes(imdbcode)) {
+                data.movies.push(imdbcode)
+            } else {
+                data.movies = data.movies.filter(movies => movies != imdbcode)
+            }
+            data.save( (err) => { console.log(err) })
+        }
+        return res.sendStatus(200)
+    })
+}
+
+const Actions = (req, res) => {
+    if (req.params.action == 'like') {
+        likeInteraction(req, res)
+    } else if (req.params.action == 'watchlist') {
+        watchList(req, res)
+    }
+}
+
+module.exports = { Actions }
