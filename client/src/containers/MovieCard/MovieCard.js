16a0d00c590c17079a29ac5675650dfddf14b037
JV <jeguglie@student.42.fr>
2020-03-07 13:54:55 +0100

    fix player

--- client/src/containers/MovieCard/MovieCard.js
+++ client/src/containers/MovieCard/MovieCard.js
@@ -385,9 +385,9 @@ export default function MovieCard(props){
                                             controls={true}
                                             config={{
                                                     file: {
-                                                        // attributes: {
-                                                        //     crossOrigin: 'true'
-                                                        // },
+                                                        attributes: {
+                                                            crossOrigin: 'use-credentials'
+                                                        },
                                                         tracks: subtitles
                                                     }
                                             }}
